1a43def7b9637b133cbd2d958cbb3079c52aec9a
summerzahara <gelb.developer@gmail.com>
2023-12-04 19:28:27 -0800

    Day 3 - Part 2

--- Day_3/d3p2.py
+++ Day_3/d3p2.py
@@ -1,14 +1,11 @@
 from icecream import ic
-import re
 
-# Parse input file
 def open_input_file():
     with open("d3_input.txt", "r") as file:
         data = [item.strip() for item in file.readlines()]
     return data
 
 
-# Update entries to make easier to parse
 def analyze(input):
     master_list = []
     for line in input:
@@ -18,14 +15,15 @@ def analyze(input):
                 line_list.append("n")
             elif char.isdigit():
                 line_list.append(char)
+            elif char == '*':
+                line_list.append(char)
             else:
                 line_list.append("y")
         master_list.append(line_list)
     # ic(master_list)
     return master_list
 
-
-# ic(analyze(test_data))
+# ic(analyze(open_input_file()))
 
 # Parse data to create a dictionary row: [all numbers] [ (start index, end index)]
 def create_index_dict(list):
@@ -49,30 +47,24 @@ def create_index_dict(list):
     # ic(all_dict)
     return all_dict
 
-# ic(create_index_dict(open_input_file()))
-
-
-# ic(catalog(open_input_file()))
-
-# Parse start in end indexes to find all indexes in range
 def tup_range(tuple):
     return list(range(tuple[0], tuple[1] + 1))
 
-
-# Get part nums
-def part_nums(master_list, dictionary):
+def gear_ratio(master_list, dictionary):
     # Get coordinates where there are 'symbols'
     all_coords = []
     for item in master_list:
         item_size = len(item)
         for i in range(item_size):
-            if item[i] == 'y':
+            if item[i] == '*':
                 coord = (master_list.index(item), i)
                 all_coords.append(coord)
     # ic(all_coords)
     total_dict = {}
+    total_ratios = []
     # check for numbers touching symbols
     for item in all_coords:
+        item_nums = []
         row_num = item[0]
         # row above
         if row_num == 0:
@@ -84,13 +76,8 @@ def part_nums(master_list, dictionary):
                 if (item[1] - 1) in index or item[1] in index or (item[1] + 1) in index:
                     entry_index = row_above[1].index(entry)
                     num = row_above[0][entry_index]
-                    if row_num - 1 in total_dict.keys():
-                        if row_above[0].count(num) == total_dict[row_num - 1].count(num):
-                            pass
-                        else:
-                            total_dict[row_num - 1].append(num)
-                    else:
-                        total_dict[row_num - 1] = [num]
+                    item_nums.append(num)
+
         # same row
         if row_num in dictionary.keys():
             row = dictionary[row_num]
@@ -99,13 +86,7 @@ def part_nums(master_list, dictionary):
                 if (item[1] - 1) in index or (item[1] + 1) in index:
                     entry_index = row[1].index(entry)
                     num = row[0][entry_index]
-                    if row_num in total_dict.keys():
-                        if row[0].count(num) == total_dict[row_num].count(num):
-                            pass
-                        else:
-                            total_dict[row_num].append(num)
-                    else:
-                        total_dict[row_num] = [num]
+                    item_nums.append(num)
         # row below
         if row_num + 1 in dictionary.keys():
             row_below = dictionary[row_num + 1]
@@ -114,25 +95,16 @@ def part_nums(master_list, dictionary):
                 if (item[1] - 1) in index or item[1] in index or (item[1] + 1) in index:
                     entry_index = row_below[1].index(entry)
                     num = row_below[0][entry_index]
-                    if row_num + 1 in total_dict.keys():
-                        if row_below[0].count(num) == total_dict[row_num + 1].count(num):
-                            pass
-                        else:
-                            total_dict[row_num + 1].append(num)
-                    else:
-                        total_dict[row_num + 1] = [num]
-    ic(total_dict)
-    sum_total = []
-    for key, value in total_dict.items():
-        sum_total.append(value)
-    sum_total = sum(sum_total, [])
-    sum_total = list(map(int, sum_total))
-    unique = set(sum_total)
-    ic(sum(sum_total))
-    ic(sum(unique))
+                    item_nums.append(num)
+        if len(item_nums) == 2:
+            # ic(item_nums)
+            ratio = 1
+            for i in item_nums:
+                ratio *= int(i)
+            total_ratios.append(ratio)
+    # ic(total_dict)
+    # ic(total_ratios)
+    sum_total = sum(total_ratios)
+    ic(sum_total)
 
-# analyze(open_input_file())
-# part_nums(analyze(open_input_file()), catalog(open_input_file()))
-part_nums(analyze(open_input_file()), create_index_dict(open_input_file()))
-
-
+gear_ratio(analyze(open_input_file()), create_index_dict(open_input_file()))
